2bc2ec074d974c46b16c07216d669fc33c27eeca
JavierPSR <jperez-serrano7668@ceuandalucia.es>
2023-11-30 16:24:41 +0100

    HOLA ESTO ES UN COMENTARIO

---
+++ Proyecto3B/src/EjercicioExamen.java
@@ -0,0 +1,11 @@
+
+public class EjercicioExamen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola me llamo Javier Pérez-Serrano Rodríguez");
+		
+	}
+
+}
